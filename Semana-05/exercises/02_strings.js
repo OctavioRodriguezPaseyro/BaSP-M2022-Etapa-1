9ae7a15757647ab595635041624e3939b6e29f50
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-04-16 23:20:09 -0300

    BaSP-2022: Strings

--- Semana-05/exercises/02_strings.js
+++ Semana-05/exercises/02_strings.js
@@ -2,15 +2,20 @@ var randomText="this is a string variable, supercalifragilistic expialidocious"
 
 var allUpperCase=randomText.toUpperCase()
 console.log("2. a.:" , allUpperCase)
+
 var firstFive=randomText.substring(0 , 4)
 console.log("2. b.:" , firstFive)
+
 var lastThree=randomText.substring(62 , 59)
 console.log("2. c.:" , lastThree)
+
 var firstCharacterUpperCase=randomText.substring(0 , 1).toUpperCase() + randomText.substring(25 , 1).toLowerCase()
 console.log("2. d.:" , firstCharacterUpperCase)
+
 var spacePosition=randomText.indexOf(" ")
 console.log("2. e.: Position of first space: " , spacePosition)
-console.log("2. f.: Position of first long word: " , randomText.indexOf("supercalifragilistic"))
-console.log("2. f.: Position of second long word: " , randomText.indexOf("expialidocious"))
+
+// console.log("2. f.: Position of first long word: " , randomText.indexOf("supercalifragilistic"))
+// console.log("2. f.: Position of second long word: " , randomText.indexOf("expialidocious"))
 var longWordsUpperCase=randomText.substring(27 , 28).toUpperCase() + randomText.substring(28 , 48).toLowerCase() + randomText.substring(48 , 49).toUpperCase() + randomText.substring(49).toLowerCase()
 console.log("2. f.:" , longWordsUpperCase)
