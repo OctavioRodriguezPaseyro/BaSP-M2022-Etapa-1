6339d43a45bce6a294695b6e9032de30a00b7b7e
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-04-17 14:07:20 -0300

    BaSP-2022: For

--- Semana-05/exercises/05_for.js
+++ Semana-05/exercises/05_for.js
@@ -1,11 +1,17 @@
 var fiveWords = ["one" , "two" , "three" , "four" , "five"]
 
-fiveWords.forEach(string => alert("5. a.: " + string))
+// fiveWords.forEach(string => alert("5. a.: " + string))
+for (var i=0 ; i <= fiveWords.length -1 ; i++){
+    alert("5. a.: " + fiveWords[i])
+}
 
-fiveWords.forEach(string => alert("5. b.: " + string.charAt(0).toUpperCase() + string.substring(1).toLowerCase()))
+// fiveWords.forEach(string => alert("5. b.: " + string.charAt(0).toUpperCase() + string.substring(1).toLowerCase()))
+for (var i=0 ; i <= fiveWords.length -1 ; i++){
+    alert("5. b.: " + fiveWords[i].substring(0 , 1).toUpperCase() + fiveWords[i].substring(1));
+}
 
 var sentence = ""
-for (var i=0; i <= fiveWords.length -1; i++){
+for (var i=0 ; i <= fiveWords.length -1 ; i++){
     sentence = sentence + " " +  fiveWords[i]
 }
 alert("5. c.: " + sentence)
