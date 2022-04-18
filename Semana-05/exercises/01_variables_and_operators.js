87097d296b5e7159d8cf53d57679d9cd6495b8bc
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-04-18 16:26:19 -0300

    BaSP-2022-W5: Variables y Operadores

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -5,11 +5,13 @@ secondNum = 14
 sumOfNum = (firstNum + secondNum)
 console.log("1. a.: The sum is:" , sumOfNum)
 
+/*********************************************************************************************/
 var stringVar1 = 'This is a string variable.'
 var stringVar2 = 'This is another string variable'
 var sumOfStrings = stringVar1 + stringVar2
 console.log('1. b.:', sumOfStrings)
 
+/*********************************************************************************************/
 var stringVar3 = 'This is yet another string'
 var stringVar4 = 'Such as this one'
 var sumOfLenght = stringVar3.length + stringVar4.length
