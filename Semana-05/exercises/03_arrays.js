d5e9120ab5ca7537cfc77e2cbdecd7d0098d0e3e
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-04-18 16:25:18 -0300

    BaSP-2022-W5: Arrays

--- Semana-05/exercises/03_arrays.js
+++ Semana-05/exercises/03_arrays.js
@@ -2,20 +2,26 @@ var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "A
 
 console.log("3. a.: 5th and 11th month:" , months[4] , months[10])
 
+/*********************************************************************************************/
 console.log("3. b.: Sorted by alphabetical order:" , months.sort())
 
+/*********************************************************************************************/
 months.unshift("Start")
 months.push("End")
 console.log("3. c.: Added 'Start' and 'End':" , months)
 
+/*********************************************************************************************/
 months.shift()
 months.pop()
 console.log("3. d.: Removed 'Start' and 'End':" , months)
 
+/*********************************************************************************************/
 console.log("3. e.: Reversed:" , months.reverse())
 
+/*********************************************************************************************/
 console.log("3. f.: Joint with '-':" , months.join("-"))
 
+/*********************************************************************************************/
 var monthsCopy = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
 
 console.log("3. g.: Sliced from month 5 to 11:" , monthsCopy.slice(4 , 11))
