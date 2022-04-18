199b0514c5a951fe6d2352c2f05df5b89d734a5a
OctavioRodriguezPaseyro <101280747+OctavioRodriguezPaseyro@users.noreply.github.com>
2022-04-18 16:22:56 -0300

    BaSP-2022-W5: Funciones

--- Semana-05/exercises/06_functions.js
+++ Semana-05/exercises/06_functions.js
@@ -1,13 +1,52 @@
+/**********************************************************/
 function sumOfNum(a , b) {
-a = 5
-b = 10
     if (typeof a === "number" && typeof b === "number"){
         return a + b
     } else {
+        alert("6. b.: Both variables must be numeric")
         return NaN
-        throw TypeError("Both variables must be numbers")
     }
 }
 
-var sumOfNumResult = sumOfNum()
+var sumOfNumResult = sumOfNum(2, 3)
 console.log("6. a.:" , sumOfNumResult)
+
+/**********************************************************/
+var sumOfNumResult = sumOfNum("a" , 3)
+console.log("6. b.:" , sumOfNumResult)
+
+/**********************************************************/
+function anInteger(c) {
+    if (Number.isInteger(c)) {
+        return true
+    } else {
+        return false
+    }
+}
+    
+var enteredValue = anInteger(1.5)
+console.log("6. c.:" , enteredValue)
+
+/**********************************************************/
+function integerSum(d , e){
+    if (typeof d !== "number" && typeof e !== "number"){
+        alert("6. d.: Both variables must be numeric")
+        return NaN
+    } else {
+        alert("6. d. & e.: Result will be rounded")
+        return Math.round(d + e)
+    }
+}
+
+var roundedValue = integerSum(1.5 , 2)
+console.log("6. d.:" , roundedValue)
+
+/**********************************************************/
+function Validation (f , g){
+    if (Number.isInteger(f && g)){
+        return f + g
+    } else {
+        return integerSum(f , g)
+    }
+}
+console.log('6. e.:', Validation(1.5 , 7.5))
